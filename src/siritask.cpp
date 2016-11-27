afb553d65654a1989b09de986146dce8b0ac4c99
Francis Banyikwa <mhogomchungu@gmail.com>
2016-11-28 01:06:02 +0300

    add support for unlocking ecryptfs in read only mode

--- src/siritask.cpp
+++ src/siritask.cpp
@@ -201,20 +201,28 @@ static QString _args( const QString& exe,const siritask::options& opt,
 			return q ;
 		} ;
 
-		if( create ){
+		auto mode = [ & ](){
 
-			auto e = QString( "%1 %2 -a %3 %4 %5" ) ;
+			if( opt.ro ){
+
+				return "--readonly" ;
+			}else{
+				return "" ;
+			}
+		}() ;
+
+		auto e = QString( "%1 %2 %3 -a %4 %5 %6" ) ;
+
+		if( create ){
 
 			auto s = _options( { "ecryptfs_passthrough=n",
 					     "ecryptfs_enable_filename_crypto=y",
 					     "ecryptfs_key_bytes=32",
 					     "ecryptfs_cipher=aes" } ) ;
 
-			return e.arg( exe,s,configPath,cipherFolder,mountPoint ) ;
+			return e.arg( exe,s,mode,configPath,cipherFolder,mountPoint ) ;
 		}else{
-			auto e = QString( "%1 %2 -a %3 %4 %5" ) ;
-
-			return e.arg( exe,_options( {} ),configPath,cipherFolder,mountPoint ) ;
+			return e.arg( exe,_options( {} ),mode,configPath,cipherFolder,mountPoint ) ;
 		}
 	}else{
 		auto e = QString( "%1 %2 %3 %4 %5 %6 -o fsname=%7@%8 -o subtype=%9" ) ;
