d6033913ef5e51eb0f77c5b210b439265b8dc9ce
ink <mhogomchungu@gmail.com>
2017-08-26 12:06:18 +0300

    fix a bug

--- src/siritask.cpp
+++ src/siritask.cpp
@@ -236,12 +236,12 @@ static QString _args( const QString& exe,const siritask::options& opt,
 
 				if( create ){
 
-					auto e = QString( "%1 --init -q -- %2 %3" ) ;
+					auto e = QString( "%1 --init -q %2 %3" ) ;
 					return e.arg( exe,configPath,cipherFolder ) ;
 				}else{
 					mode += " -o fsname=gocryptfs@" + cipherFolder ;
 
-					auto e = QString( "%1 -q %2 %3 -- %4 %5" ) ;
+					auto e = QString( "%1 -q %2 %3 %4 %5" ) ;
 					return e.arg( exe,mode,configPath,cipherFolder,mountPoint ) ;
 				}
 			}else{
