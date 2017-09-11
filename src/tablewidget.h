fd1cc6893cad5577b6a35f0e9cd06b0c806b8b3a
ink <mhogomchungu@gmail.com>
2017-09-12 01:32:55 +0300

    remove unused code

--- src/tablewidget.h
+++ src/tablewidget.h
@@ -29,12 +29,10 @@ class QTableWidgetItem ;
 
 namespace tablewidget
 {
-	void addRow( QTableWidget *,const std::initializer_list< QString >&,const QFont& = QFont() ) ;
 	void addRow( QTableWidget *,const QStringList&,const QFont& = QFont() ) ;
 	int  addRow( QTableWidget * ) ;
 
 	void updateRow( QTableWidget *,const QStringList&,int row,const QFont& = QFont() ) ;
-	void updateRow( QTableWidget *,const std::initializer_list< QString >&,int row,const QFont& ) ;
 
 	void setFont( QTableWidget *,int row,const QFont& ) ;
 
