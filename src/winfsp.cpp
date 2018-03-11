2b6794087e18e69104f948b44b3ae69c28a82788
ink <mhogomchungu@gmail.com>
2018-03-12 00:34:05 +0300

    a bit of refactoring

--- src/winfsp.cpp
+++ src/winfsp.cpp
@@ -19,18 +19,12 @@
 
 #include "winfsp.h"
 #include "utility.h"
+#include "winfsp_impl.hpp"
 
 #include <utility>
 
 namespace SiriKali{
 namespace Winfsp{
-
-struct winFsp{
-
-	QString className ;
-	QString instanceName ;
-	QString command ;
-} ;
 
 class manageInstances
 {
@@ -43,19 +37,6 @@ private:
 	std::pair< bool,QByteArray > waitForStarted( QProcess * ) ;
 	std::vector< QProcess * > m_instances ;
 	std::function< void() > m_updateVolumeList ;
-} ;
-
-class ActiveInstances
-{
-public:
-	ActiveInstances() ;
-	~ActiveInstances() ;
-	bool valid() const ;
-	const std::vector< SiriKali::Winfsp::winFsp >& values() const ;
-	std::vector< QStringList > commands() const ;
-private:
-	class impl ;
-	std::unique_ptr< impl > m_handle ;
 } ;
 
 static SiriKali::Winfsp::manageInstances _winfsInstances ;
@@ -170,178 +151,6 @@ QString SiriKali::Winfsp::readRegister( const char * path,const char * key )
 
 	return QString() ;
 }
-
-#endif
-
-//#ifdef Q_OS_WIN
-#if 0
-#include <winfsp/launch.h>
-#include <Windows.h>
-#include <Winreg.h>
-
-#include <array>
-#include <algorithm>
-
-class SiriKali::Winfsp::ActiveInstances::impl
-{
-public:
-	impl()
-	{
-		for( const auto& it : this->nameList() ){
-
-			this->addToList( it ) ;
-		}
-	}
-	bool valid() const
-	{
-		return m_error == 0 ;
-	}
-	const std::vector< SiriKali::Winfsp::winFsp >& values() const
-	{
-		return m_entries ;
-	}
-	std::vector< QStringList > commands() const
-	{
-		return {} ;
-	}
-private:
-	void addToList( const SiriKali::Winfsp::winFsp& e )
-	{
-		std::array< WCHAR,BUFFER_SIZE > buffer ;
-
-		auto size = static_cast< ULONG >( buffer.size() ) ;
-
-		std::array< WCHAR,BUFFER_SIZE > className ;
-		std::array< WCHAR,BUFFER_SIZE > instanceName ;
-
-		e.className.toWCharArray( className.data() ) ;
-		e.instanceName.toWCharArray( instanceName.data() ) ;
-
-		FspLaunchGetInfo( className.data(),
-				  instanceName.data(),
-				  buffer.data(),
-				  &size,
-				  &m_error) ;
-
-		size = size / sizeof( WCHAR ) ;
-
-		if( m_error == 0 ){
-
-			this->build( m_entries,buffer.data(),size,3 ) ;
-		}
-	}
-	std::vector< SiriKali::Winfsp::winFsp > nameList()
-	{
-		std::array< WCHAR,BUFFER_SIZE > buffer ;
-
-		auto size = static_cast< ULONG >( buffer.size() ) ;
-
-		std::vector< SiriKali::Winfsp::winFsp > entries ;
-
-		FspLaunchGetNameList( buffer.data(),&size,&m_error) ;
-
-		size = size / sizeof( WCHAR ) ;
-
-		if( m_error == 0 ){
-
-			this->build( entries,buffer.data(),size,2 ) ;
-		}
-
-		return entries ;
-	}
-	std::pair< WCHAR *,ULONG > component( WCHAR * buffer,ULONG size,ULONG e )
-	{
-		ULONG i = e ;
-
-		while( true ){
-
-			if( e >= size ){
-
-				return { nullptr,0 } ;
-
-			}else if( buffer[ e ] == 0 ){
-
-				return { &buffer[ i ],e + 1 } ;
-			}else{
-				e++ ;
-			}
-		}
-	}
-	void build( std::vector< SiriKali::Winfsp::winFsp >& entries,
-		     WCHAR * buffer,ULONG size,int count )
-	{
-		ULONG i = 0 ;
-
-		while( true ){
-
-			auto s = this->component( buffer,size,i ) ;
-
-			if( s.first == nullptr ){
-
-				return ;
-			}
-
-			auto e = this->component( buffer,size,s.second ) ;
-
-			if( e.first == nullptr ){
-
-				return ;
-			}
-
-			if( count == 2 ){
-
-				auto a = QString::fromWCharArray( s.first ) ;
-				auto b = QString::fromWCharArray( e.first ) ;
-
-				entries.emplace_back( SiriKali::Winfsp::winFsp{ a,b,QString() } ) ;
-
-				i = e.second ;
-			}else{
-				auto m = this->component( buffer,size,e.second ) ;
-
-				if( m.first == nullptr ){
-
-					return ;
-				}
-
-				auto a = QString::fromWCharArray( s.first ) ;
-				auto b = QString::fromWCharArray( e.first ) ;
-				auto c = QString::fromWCharArray( m.first ) ;
-
-				entries.emplace_back( SiriKali::Winfsp::winFsp{ a,b,c } ) ;
-
-				i = m.second ;
-			}
-		}
-	}
-	static const size_t BUFFER_SIZE = 4096 ;
-	ULONG m_error ;
-	std::vector< SiriKali::Winfsp::winFsp > m_entries ;
-} ;
-
-#else
-
-class SiriKali::Winfsp::ActiveInstances::impl
-{
-public:
-	impl()
-	{
-	}
-	bool valid() const
-	{
-		return false ;
-	}
-	const std::vector< SiriKali::Winfsp::winFsp >& values() const
-	{
-		return m_entries ;
-	}
-	std::vector< QStringList > commands() const
-	{
-		return {} ;
-	}
-private:
-	std::vector< SiriKali::Winfsp::winFsp > m_entries ;
-};
 
 #endif
 
