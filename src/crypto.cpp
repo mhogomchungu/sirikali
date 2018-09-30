b868bae21d0a099a9287283a658f66dd143553a6
ink <mhogomchungu@gmail.com>
2018-09-30 21:30:25 +0300

    reduce dependency on libgcrypt to make it possible to eventually remove it as a dependency on windows

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -25,10 +25,46 @@
 #include <gcrypt.h>
 #pragma GCC diagnostic warning "-Wdeprecated-declarations"
 
-class cryptoImpl
+#include <random>
+
+class rng{
+public:
+	rng()
+	{
+		m_rng.seed( m_randomDevice() ) ;
+	}
+	QByteArray get( int s )
+	{
+		std::uniform_int_distribution<> getRandom( 0,255 ) ;
+
+		QByteArray m ;
+
+		m.resize( s ) ;
+
+		auto e = m.data() ;
+
+		for( int i = 0 ; i < s ; i++ ){
+
+			*( e + i ) = static_cast< char >( getRandom( m_rng ) ) ;
+		}
+
+		return m ;
+	}
+private:
+	std::mt19937 m_rng ;
+	std::random_device m_randomDevice ;
+} ;
+
+QByteArray crypto::getRandomData( int s )
+{
+	static thread_local rng m ;
+	return m.get( s ) ;
+}
+
+class libgcrypt
 {
 public:
-	cryptoImpl( int algo,unsigned int flags,const QString& key = QString() )
+	libgcrypt( int algo,unsigned int flags,const QString& key = QString() )
 	{
 		this->init_grypt() ;
 
@@ -40,12 +76,10 @@ public:
 			this->setKey( key ) ;
 		}
 	}
-
-	cryptoImpl()
+	libgcrypt()
 	{
 		this->init_grypt() ;
 	}
-
 	QByteArray getRandomData( int s )
 	{
 		QByteArray buffer ;
@@ -57,29 +91,24 @@ public:
 
 		return buffer ;
 	}
-
 	void write( const void * buffer,size_t size )
 	{
 		gcry_md_write( m_handle,buffer,size ) ;
 	}
-
 	void write( const QByteArray& buffer )
 	{
 		gcry_md_write( m_handle,buffer.constData(),static_cast< size_t >( buffer.size() ) ) ;
 	}
-
 	bool setKey( const QString& e )
 	{
 		m_error_status = gcry_md_setkey( m_handle,e.toLatin1().constData(),static_cast< size_t >( e.size() ) ) ;
 
 		return m_error_status == GPG_ERR_NO_ERROR ;
 	}
-
 	operator bool()
 	{
 		return m_error_status == GPG_ERR_NO_ERROR ;
 	}
-
 	QByteArray result()
 	{
 		auto key = reinterpret_cast< const char * >( gcry_md_read( m_handle,0 ) ) ;
@@ -87,35 +116,8 @@ public:
 
 		return { key,len } ;
 	}
-
-	~cryptoImpl()
+	QByteArray hmack_key( const QString& keyFile )
 	{
-		gcry_md_close( m_handle ) ;
-	}
-private:
-	void init_grypt()
-	{
-		if( gcry_control( GCRYCTL_INITIALIZATION_FINISHED_P ) == 0 ){
-
-			gcry_check_version( nullptr ) ;
-			gcry_control( GCRYCTL_INITIALIZATION_FINISHED,0 ) ;
-		}
-	}
-
-	int m_algo ;
-	unsigned int m_error_status ;
-	gcry_md_hd_t m_handle = nullptr ;
-};
-
-QByteArray crypto::getRandomData( int s )
-{
-	return cryptoImpl().getRandomData( s ) ;
-}
-
-QByteArray crypto::hmac_key(const QString& keyFile,const QString& password )
-{
-	auto _getKey = []( cryptoImpl& handle,const QString& keyFile ){
-
 		QFile f( keyFile ) ;
 
 		if( f.open( QIODevice::ReadOnly ) ){
@@ -128,24 +130,44 @@ QByteArray crypto::hmac_key(const QString& keyFile,const QString& password )
 				auto e = f.read( buffer,size ) ;
 
 				if( e <= 0 ){
+
 					break ;
 				}else{
-					handle.write( buffer,static_cast< size_t >( e ) ) ;
+					this->write( buffer,static_cast< size_t >( e ) ) ;
 				}
 			}
 
-			return handle.result().toHex() ;
+			return this->result().toHex() ;
 		}else{
 			return QByteArray() ;
 		}
-	} ;
+	}
+	~libgcrypt()
+	{
+		gcry_md_close( m_handle ) ;
+	}
+private:
+	void init_grypt()
+	{
+		if( gcry_control( GCRYCTL_INITIALIZATION_FINISHED_P ) == 0 ){
 
-	cryptoImpl handle( GCRY_MD_SHA256,GCRY_MD_FLAG_HMAC,password ) ;
+			gcry_check_version( nullptr ) ;
+			gcry_control( GCRYCTL_INITIALIZATION_FINISHED,0 ) ;
+		}
+	}
+	int m_algo ;
+	unsigned int m_error_status ;
+	gcry_md_hd_t m_handle = nullptr ;
+};
+
+QByteArray crypto::hmac_key(const QString& keyFile,const QString& password )
+{
+	static thread_local libgcrypt handle( GCRY_MD_SHA256,GCRY_MD_FLAG_HMAC,password ) ;
 
 	if( handle ){
 
-		return _getKey( handle,keyFile ) ;
+		return handle.hmack_key( keyFile ) ;
 	}
 
-	return QByteArray() ;
+	return {} ;
 }
