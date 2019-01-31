ad6eef7d0c404000a39bd0d9cf258099294fb10a
ink <mhogomchungu@gmail.com>
2019-01-31 21:45:20 +0300

    reduce dependency on libgcrypt

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -18,12 +18,10 @@
  */
 
 #include "crypto.h"
+#include "task.hpp"
 
 #include <QFile>
-
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#include <gcrypt.h>
-#pragma GCC diagnostic warning "-Wdeprecated-declarations"
+#include <QMessageAuthenticationCode>
 
 #include <random>
 
@@ -61,113 +59,22 @@ QByteArray crypto::getRandomData( int s )
 	return m.get( s ) ;
 }
 
-class libgcrypt
+QByteArray crypto::hmac_key( const QString& keyFile,const QString& password )
 {
-public:
-	libgcrypt( int algo,unsigned int flags,const QString& key = QString() )
-	{
-		this->init_grypt() ;
+	return Task::await( [ & ](){
 
-		m_algo = algo ;
-		m_error_status = gcry_md_open( &m_handle,algo,flags ) ;
+		QMessageAuthenticationCode hmac( QCryptographicHash::Sha256,password.toLatin1() ) ;
 
-		if( m_error_status == GPG_ERR_NO_ERROR ){
+		QFile file( keyFile ) ;
 
-			this->setKey( key ) ;
-		}
-	}
-	libgcrypt()
-	{
-		this->init_grypt() ;
-	}
-	QByteArray getRandomData( int s )
-	{
-		QByteArray buffer ;
-		buffer.resize( s ) ;
+		if( file.open( QIODevice::ReadOnly ) ){
 
-		gcry_randomize( buffer.data(),
-				static_cast< unsigned long >( buffer.size() ),
-				GCRY_STRONG_RANDOM ) ;
+			if( hmac.addData( &file ) ){
 
-		return buffer ;
-	}
-	void write( const void * buffer,size_t size )
-	{
-		gcry_md_write( m_handle,buffer,size ) ;
-	}
-	void write( const QByteArray& buffer )
-	{
-		gcry_md_write( m_handle,buffer.constData(),static_cast< size_t >( buffer.size() ) ) ;
-	}
-	bool setKey( const QString& e )
-	{
-		m_error_status = gcry_md_setkey( m_handle,e.toLatin1().constData(),static_cast< size_t >( e.size() ) ) ;
-
-		return m_error_status == GPG_ERR_NO_ERROR ;
-	}
-	operator bool()
-	{
-		return m_error_status == GPG_ERR_NO_ERROR ;
-	}
-	QByteArray result()
-	{
-		auto key = reinterpret_cast< const char * >( gcry_md_read( m_handle,0 ) ) ;
-		auto len = static_cast< int >( gcry_md_get_algo_dlen( m_algo ) ) ;
-
-		return { key,len } ;
-	}
-	QByteArray hmack_key( const QString& keyFile )
-	{
-		QFile f( keyFile ) ;
-
-		if( f.open( QIODevice::ReadOnly ) ){
-
-			const int size = 1024 ;
-			char buffer[ size ] ;
-
-			while( true ){
-
-				auto e = f.read( buffer,size ) ;
-
-				if( e <= 0 ){
-
-					break ;
-				}else{
-					this->write( buffer,static_cast< size_t >( e ) ) ;
-				}
+				return hmac.result().toHex() ;
 			}
-
-			return this->result().toHex() ;
-		}else{
-			return QByteArray() ;
 		}
-	}
-	~libgcrypt()
-	{
-		gcry_md_close( m_handle ) ;
-	}
-private:
-	void init_grypt()
-	{
-		if( gcry_control( GCRYCTL_INITIALIZATION_FINISHED_P ) == 0 ){
 
-			gcry_check_version( nullptr ) ;
-			gcry_control( GCRYCTL_INITIALIZATION_FINISHED,0 ) ;
-		}
-	}
-	int m_algo ;
-	unsigned int m_error_status ;
-	gcry_md_hd_t m_handle = nullptr ;
-};
-
-QByteArray crypto::hmac_key(const QString& keyFile,const QString& password )
-{
-	static thread_local libgcrypt handle( GCRY_MD_SHA256,GCRY_MD_FLAG_HMAC,password ) ;
-
-	if( handle ){
-
-		return handle.hmack_key( keyFile ) ;
-	}
-
-	return {} ;
+		return QByteArray() ;
+	} ) ;
 }
